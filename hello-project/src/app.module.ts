a3ecc78e5e2ec597397afa186edd5c309b2b8fa8
MohsenFayyaz <mohsen.fayyaz77@gmail.com>
2020-12-11 13:11:26 +0330

    Integrate everything with app.module.ts

--- hello-project/src/app.module.ts
+++ hello-project/src/app.module.ts
@@ -1,17 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { HelloModule } from './hello/hello.module';
-import { BookModule } from './book/book.module';
-import { BooksService } from './books/books.service';
-import { BooksController } from './books/books.controller';
-import { GenreModule } from './genre/genre.module';
-import { UserModule } from './user/user.module';
-import { BooksModule } from './books/books.module';
+import { UserModule } from './User/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import UserEntity from './db/entity/user.entity';
+import BooksModule from './Books/books.module';
+import GenreModule from './Genre/genre.module';
+import BookEntity from './db/entity/book.entity';
+import GenreEntity from './db/entity/genre.entity';
 
 @Module({
-  imports: [HelloModule, BookModule, GenreModule, UserModule, BooksModule],
-  controllers: [AppController, BooksController],
-  providers: [AppService, BooksService],
+  imports: [
+    UserModule,
+    BooksModule,
+    GenreModule,
+    TypeOrmModule.forFeature(
+      [UserEntity, BookEntity, GenreEntity],
+    ),
+
+    TypeOrmModule.forRoot(),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
