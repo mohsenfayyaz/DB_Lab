0b82adf84f81a0fb95679853d2bf15fd466a4583
MohsenFayyaz <mohsen.fayyaz77@gmail.com>
2020-12-13 18:38:49 +0330

    Implement update and delete for genre

--- hello-project/src/genre/genre.controller.ts
+++ hello-project/src/genre/genre.controller.ts
@@ -1,6 +1,8 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post, Put, Query } from '@nestjs/common';
 import GenreServices from './genre.service';
 import CreateGenreDto from './dto/create-genre.dto';
+import { ApiResponse, ApiBearerAuth, ApiQuery } from '@nestjs/swagger';
+import UpdateGenreDto from './dto/update-genre.dto';
 
 @Controller('genre')
 export default class GenreController {
@@ -13,5 +15,25 @@ export default class GenreController {
     @Get()
     getAll() {
         return this.genreServices.getAllGenre();
+    }
+
+    @ApiResponse({ status: 200, description: "Delete Genre" })
+    @ApiBearerAuth()
+    @ApiQuery({
+        name: 'genreID',
+        required: true,
+        type: Number,
+        description :`id of genre being deleted`
+    })
+    @Delete('delete')
+    deleteBook(@Query('genreID') genreID) {
+        return this.genreServices.delete(genreID);
+    }
+
+    @ApiResponse({ status: 200, description: "Update Genre" })
+    @ApiBearerAuth()
+    @Put('update')
+    updateBook(@Body() genre: UpdateGenreDto) {
+        return this.genreServices.update(genre);
     }
 }
